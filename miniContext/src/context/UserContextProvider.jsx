ff1874037faf3d8b057e7b5c54e991f287affcd9
sarveshhome <sarvesh.carlton@gmail.com>
2025-08-17 15:09:34 +0530

    theme switcher with context api

--- miniContext/src/context/UserContextProvider.jsx
+++ miniContext/src/context/UserContextProvider.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import UserContext from "./UserContext";
+import React from 'react';
+import UserContext from './UserContext';
 
 const UserContextProvider = ({ children }) => {
-    const [user, setUser] = React.useState(null);
+  const [user, setUser] = React.useState(null);
 
-    return(
-        <UserContext.Provider value={{user,setUser}}>
-          {children}
-        </UserContext.Provider>
-    )
-}
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContextProvider;
